bc8fb1a2c82c21c0bd860a84ab132cfc97f68d29
kwakminoo <kwakminoo@github.com>
2025-11-27 12:23:49 +0900

    fix: robots.ts?먯꽌 disallow瑜?鍮?諛곗뿴濡?紐낆떆?곸쑝濡??ㅼ젙?섏뿬 紐⑤뱺 ?щ·???묎렐 ?덉슜

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [],
+      },
+    ],
     sitemap: 'https://www.onsoom.co.kr/sitemap.xml',
   };
 }
